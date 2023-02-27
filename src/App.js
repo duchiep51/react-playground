42e6541301498ab37d568b60c64e30d6b05a7384
Hiep Tran <hiep.tran5198@gmail.com>
2023-02-27 18:33:28 +0700

    play with react-toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import SimulateBugWithLib from "./components/atoms/ErrorBoundary/ErrorBoundaryLib";
+
+const Msg = ({ closeToast, toastProps }) => (
+  <div>
+    Lorem ipsum dolor {toastProps.position}
+    <button>Retry</button>
+    <button onClick={closeToast}>Close</button>
+  </div>
+)
+
 
 function App() {
   const [state, setState] = useState(() => 1);
   const [userId, setUserId] = useState("1");
+  const notify = () => {
+    // toast("Wow so easy !");
+
+    toast.success(<Msg />, {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
 
   // console.log('----------------------')
   // console.log('outside')
@@ -24,6 +42,7 @@ function App() {
       <HoverIncrease /> */}
       {/* <LoginForm /> */}
       <SimulateBugWithLib />
+      <button onClick={notify}>Notify !</button>
     </div>
   );
 }
